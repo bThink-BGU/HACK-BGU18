3971cdc196e48ca673a555a795127bd23d01f48e
Achiya Elyasaf <achiya.e@gmail.com>
2018-03-23 00:37:13 +0200

    added firestore

--- dashboard/src/app/layout/layout-routing.module.ts
+++ dashboard/src/app/layout/layout-routing.module.ts
@@ -9,6 +9,7 @@ const routes: Routes = [
         children: [
             { path: '', redirectTo: 'dashboard' },
             { path: 'dashboard', loadChildren: './dashboard/dashboard.module#DashboardModule' },
+            { path: 'profile', loadChildren: './profile/profile.module#ProfileModule' },
             { path: 'charts', loadChildren: './charts/charts.module#ChartsModule' },
             { path: 'tables', loadChildren: './tables/tables.module#TablesModule' },
             { path: 'forms', loadChildren: './form/form.module#FormModule' },
